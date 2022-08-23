8059d42384df570d5ce8385304c3e1853b3248fd
waterfallstreamair <waterfallstreamair@gmail.com>
2022-08-23 13:48:29 +0000

    Add setup-vnc.sh

---
+++ setup-vnc.sh
@@ -0,0 +1,25 @@
+sudo apt-get install ubuntu-gnome-desktop
+sudo apt-get install tightvncserver xtightvncviewer tightvnc-java
+sudo locale-gen de_DE.UTF-8
+sudo apt-get install xfonts-75dpi
+sudo apt-get install xfonts-100dpi
+sudo apt-get install gnome-panel
+sudo apt-get install metacity
+sudo apt-get install light-themes
+touch ~/.Xresources
+// vncpasswd
+
+sudo apt-get install metacity gnome-panel
+sudo apt-get install indicator-applet-complete
+sudo apt-get install gnome-session-flashback
+sudo apt-get install unity-settings-daemon
+
+sudo locale-gen
+sudo localectl set-locale LANG="en_US.UTF-8"
+
+gsettings set org.gnome.desktop.background show-desktop-icons true
+sudo apt-get install ubuntu-settings
+
+
+
+#sudo reboot
